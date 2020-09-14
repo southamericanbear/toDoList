14a60e0e109041268d29136b1f4b92329e6d233d
alan Dittler <dittler.a@gmail.com>
2020-09-13 22:52:41 -0300

    changed the weather api to the one from OpenWeather and made it work with the geolocation

--- scripts/date-and-weather.js
+++ scripts/date-and-weather.js
@@ -9,47 +9,47 @@ const daOfTheWeek = new Intl.DateTimeFormat("en", { weekday: "long" }).format(
 today.innerHTML = `${da} ${daOfTheWeek} ${mo}`;
 // weather stuff
 
-const key = "cYzmTT27MASzU3kEs1eZJQzOdgouEDt7";
-const getTheCity = document.querySelector(".gettheweather");
-const injectweather = document.querySelector(".weather");
-const injectNameCity = document.querySelector(".nameOfCity");
+// // UI manupulating API
+// const key = "cYzmTT27MASzU3kEs1eZJQzOdgouEDt7";
+// const getTheCity = document.querySelector(".gettheweather");
+// const injectweather = document.querySelector(".weather");
+// const injectNameCity = document.querySelector(".nameOfCity");
 
-const updateUI = (data) => {
-  const cityDets = data.cityDets;
-  const weather = data.weather;
-  injectNameCity.innerHTML = `<h3 class="nameOfCity">${cityDets.EnglishName}</h3>`;
-  injectweather.innerHTML = `<span class="weather">${weather.Temperature.Metric.Value}&deg;C</span>`;
-};
+// const updateUI = (data) => {
+//   const { cityDets, weather } = data;
+//   injectNameCity.innerHTML = `<h3 class="nameOfCity">${cityDets.EnglishName}</h3>`;
+//   injectweather.innerHTML = `<span class="weather">${weather.Temperature.Metric.Value}&deg;C</span>`;
+// };
 
-const updateCity = async (city) => {
-  const cityDets = await getCity(city);
-  const weather = await getWeather(cityDets.Key);
+// const updateCity = async (city) => {
+//   const cityDets = await getCity(city);
+//   const weather = await getWeather(cityDets.Key);
 
-  return { cityDets, weather };
-};
+//   return { cityDets, weather };
+// };
 
-getTheCity.addEventListener("submit", (e) => {
-  e.preventDefault();
-  let city = getTheCity.gettheweather.value.trim();
-  getTheCity.reset();
-  updateCity(city)
-    .then((data) => console.log(updateUI(data)))
-    .catch((err) => console.log(err));
-});
+// getTheCity.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   let city = getTheCity.gettheweather.value.trim();
+//   getTheCity.reset();
+//   updateCity(city)
+//     .then((data) => console.log(updateUI(data)))
+//     .catch((err) => console.log(err));
+// });
 
-// async and get the calls for the API stuff
-const getWeather = async (id) => {
-  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
-  const query = `${id}?apikey=${key}`;
-  const response = await fetch(base + query);
-  const data = await response.json();
-  return data[0];
-};
+// // async and get the calls for the API stuff
+// const getWeather = async (id) => {
+//   const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+//   const query = `${id}?apikey=${key}`;
+//   const response = await fetch(base + query);
+//   const data = await response.json();
+//   return data[0];
+// };
 
-const getCity = async (city) => {
-  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
-  const query = `?apikey=${key}&q=${city}`;
-  const response = await fetch(base + query);
-  const data = await response.json();
-  return data[0];
-};
+// const getCity = async (city) => {
+//   const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+//   const query = `?apikey=${key}&q=${city}`;
+//   const response = await fetch(base + query);
+//   const data = await response.json();
+//   return data[0];
+// };
---
+++ scripts/geolocationScript.js
@@ -0,0 +1,33 @@
+const injectWeather = document.querySelector(".injectWeather");
+
+const getWeather = () => {
+  let injectCity = document.querySelector(".nameOfCity");
+  let injectWeather = document.querySelector(".weather");
+  let api = "https://api.openweathermap.org/data/2.5/weather";
+  let apiKey = "0fd94cd813d08a17339fcf01cac3d7c6";
+
+  navigator.geolocation.getCurrentPosition(success, error);
+
+  function success(position) {
+    let latitud = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    let url = `${api}?lat=${latitud}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        let temp = data.main.temp;
+        let town = data.name;
+        injectCity.innerHTML = `<h3 class="nameOfCity">${town}</h3>`;
+        injectWeather.innerHTML = `<span class="weather">${temp}&deg;C</span>`;
+      })
+      .catch((err) => console.log(err, "error"));
+  }
+
+  function error() {
+    console.log("error");
+  }
+};
+
+getWeather();
