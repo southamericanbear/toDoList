2eed36365f31b4f855ee0985155d70b31d3e538f
alan Dittler <dittler.a@gmail.com>
2020-09-14 22:56:20 -0300

    change the code of the todolist to get  a better performance

--- scripts/date-and-weather.js
+++ scripts/date-and-weather.js
@@ -2,11 +2,11 @@
 const now = new Date();
 const today = document.querySelector("h1");
 const da = new Intl.DateTimeFormat("en", { day: "numeric" }).format(now);
-const mo = new Intl.DateTimeFormat("en", { month: "long" }).format(now);
+const mo = new Intl.DateTimeFormat("en", { month: "short" }).format(now);
 const daOfTheWeek = new Intl.DateTimeFormat("en", { weekday: "long" }).format(
   now
 );
-today.innerHTML = `${da} ${daOfTheWeek} ${mo}`;
+today.innerHTML = `${daOfTheWeek} ${da} ${mo}.`;
 // weather stuff
 
 // // UI manupulating API
--- scripts/todos-function.js
+++ scripts/todos-function.js
@@ -1,39 +1,101 @@
+// const addForm = document.querySelector(".add");
+// const list = document.querySelector(".todos");
+// const search = document.querySelector(".search input");
+// // var todos = [];
+// // var todosString = localStorage.getItem("todos");
+// // var todos = JSON.parse(todosString);
+
+// // generate new toDo's
+// const generateTemplate = (todos) => {
+//   let html = ` <li class="list-group-item d-flex justify-content-between align-items-center">
+//     <span>${todos}</span><i class="far fa-trash-alt delete"></i>
+//   </li>`;
+//   list.innerHTML += html;
+// };
+
+// // todos.forEach((todo) => generateTemplate(todo));
+
+// // submit the todo
+// addForm.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   let todo = addForm.add.value.trim();
+//   if (todo.length) {
+//     // todos.push(todo);
+//     // localStorage.setItem("todos", JSON.stringify(todos));
+//     generateTemplate(todo);
+//     addForm.reset();
+//   }
+// });
+
+// // delete todo's
+
+// list.addEventListener("click", (e) => {
+//   if (e.target.classList.contains("delete")) {
+//     e.target.parentElement.remove();
+//
+//   }
+// });
+
 const addForm = document.querySelector(".add");
 const list = document.querySelector(".todos");
 const search = document.querySelector(".search input");
-// var todos = [];
-// var todosString = localStorage.getItem("todos");
-// var todos = JSON.parse(todosString);
+let listTodo, id;
 
-// generate new toDo's
-const generateTemplate = (todos) => {
-  let html = ` <li class="list-group-item d-flex justify-content-between align-items-center">
-    <span>${todos}</span><i class="far fa-trash-alt delete"></i>
-  </li>`;
-  list.innerHTML += html;
-};
+let data = localStorage.getItem("TODO");
+if (data) {
+  listTodo = JSON.parse(data);
+  id = listTodo.length;
+  loadList(listTodo);
+} else {
+  listTodo = [];
+  id = 0;
+}
 
-// todos.forEach((todo) => generateTemplate(todo));
+function loadList(array) {
+  array.forEach(function (item) {
+    addTodo(item.name, item.id, item.trash);
+  });
+}
 
-// submit the todo
+function addTodo(todo, id, trash) {
+  if (trash) {
+    return;
+  }
+
+  let html = ` <li class="list-group-item d-flex justify-content-between align-items-center"><span>${todo}</span><i class="far fa-trash-alt delete" job="delete" id=${id}></i></li>`;
+
+  const position = "beforeend";
+  list.insertAdjacentHTML(position, html);
+}
+
 addForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  let todo = addForm.add.value.trim();
+  const todo = addForm.add.value.trim();
   if (todo.length) {
-    // todos.push(todo);
-    // localStorage.setItem("todos", JSON.stringify(todos));
-    generateTemplate(todo);
-    addForm.reset();
+    addTodo(todo, id, false);
+    listTodo.push({
+      name: todo,
+      id: id,
+      trash: false,
+    });
   }
+  localStorage.setItem("TODO", JSON.stringify(listTodo));
+  id++;
+  addForm.reset();
 });
 
-// delete todo's
+function removeTodo(element) {
+  element.parentNode.parentNode.removeChild(element.parentNode);
+  listTodo[element.id].trash = true;
+}
 
-list.addEventListener("click", (e) => {
-  if (e.target.classList.contains("delete")) {
-    e.target.parentElement.remove();
-    localStorage.setItem("todos", JSON.stringify(todos));
+list.addEventListener("click", function (event) {
+  const element = event.target;
+  const elementJOB = element.attributes.job.value;
+  if (elementJOB == "delete") {
+    removeTodo(element);
   }
+  localStorage.setItem("TODO", JSON.stringify(listTodo));
 });
 
 // filter the toDo's
